020a17e538614902a6b101fefd984c6783b13dba
jdromero88 <jdromerov88@gmail.com>
2020-04-10 16:30:57 -0400

    Game controller updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,62 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @game = Game.create(game_strong_params)
+    render json: @game
+  end
+
+  def show
+    @game = Game.find params[:id]
+    if @game
+      render json: @game
+    else
+      render json: {
+        error:true,
+        message:'No Games to show.'
+      }
+    end
+  end
+
+  def update
+    @game = Game.find params[:id]
+    if @game
+      @game.update!(game_strong_params)
+      render json: @game
+    else
+      render json:{
+        error: true,
+        message: 'Something went wrong.'
+      }
+    end
+  end
+
+  def destroy
+    @game = Game.find params[:id]
+    if @game
+      @game.destroy
+      render json:{
+        error:false,
+        message: 'Game deleted'
+      }
+    else
+      render json:{
+        error:true,
+        message: 'Something went wrong.'
+      }
+    end
+  end
+
+  private
+    def game_strong_params
+      params.require(:games).permit(
+        :location,
+        :time,
+        :result,
+        :sport
+      )
+    end
 end
